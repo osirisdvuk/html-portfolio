5b5da33e304e8b7006b91a2a1926d31e0b0f59c4
Osiris Des Djimaase De Vidoley <osirisdvuk@icloud.com>
2025-08-11 00:01:54 -0400

    Backend Tools

---
+++ Backend/HTTP Requests/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Hello World! About Us Page');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Hello World! Contact Us Page');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
+
